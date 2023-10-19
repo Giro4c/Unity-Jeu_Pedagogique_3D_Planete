21107af56ff81d9c9a77c21c984808bd3ffb80df
Maxime <115540245+JAURAS-Maxime-2225046a@users.noreply.github.com>
2023-10-19 17:07:38 +0200

    Pause menu
    implementation of a pause menu with the interaction of different buttons. Additionally, we can return to the home page

---
+++ Main_Jeu_Planete/Assets/PauseMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Home(string _sceneName)
+    {
+        SceneManager.LoadScene(_sceneName);
+        Time.timeScale = 1;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
