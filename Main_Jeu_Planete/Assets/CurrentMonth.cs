ed19316c0e0cb97186afe8621d13152e449ea76d
Yazid-Raoul Maoulida Attoumani <yazid-raoul.MAOULIDA-ATTOUMANI@etu.univ-amu.fr>
2023-10-25 23:02:15 +0200

    Slider Drag et Slider Auto

--- Main_Jeu_Planete/Assets/CurrentMonth.cs
+++ Main_Jeu_Planete/Assets/CurrentMonth.cs
@@ -13,13 +13,64 @@ public class CurrentMonth : MonoBehaviour
     [SerializeField] TMP_Text displayText; // Référence à l'objet TMP_Text
     [SerializeField]  Slider textSlider;
     public Orbit valueSlider;
+    public OrbitMotion sphere ; 
+    public OrbitDrag planete;
+    public bool isTrue;
     public string[] mois;
 
 
     void Update()
     {
+            if(Input.GetKeyDown("l"))
+            {
+                isTrue = true;
+            }
+            else if (Input.GetKeyDown("k"))
+            {
+                isTrue = false;
+            }
+            if(isTrue == true)
+            {
+                SliderDrag();
+                float orbitSpeed = 1f / valueSlider.orbitPeriod;
+                valueSlider.orbitProgress -=Time.deltaTime * orbitSpeed; 
+            }
+            else
+            {
+                SliderAuto();
+            }
+       
+       
+    }
+
+    private void SliderAuto()
+    {
         float sliderValue = valueSlider.orbitProgress;
         textSlider.value = sliderValue;
+
+        for (int i = 0; i < mois.Length; i++)
+        {
+            float lowerBound = i / (float)mois.Length;
+            float upperBound = (i + 1) / (float)mois.Length;
+
+            if (sliderValue >= lowerBound && sliderValue <= upperBound)
+            {
+                displayText.text = mois[i];
+                break; // Sort de la boucle une fois que le mois est trouvé
+            }
+        }
+    }
+    
+    private void SliderDrag()
+    {
+        // Lisez la valeur actuelle du slider
+        float sliderValue = textSlider.value;
+        valueSlider.orbitProgress = sliderValue;
+        /*if(planete.orbitActive)
+        {
+            planete.DragOrbit();
+        }*/
+        
 
         for (int i = 0; i < mois.Length; i++)
         {
