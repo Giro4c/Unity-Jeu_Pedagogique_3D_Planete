a9ed1a3668e7ffd1655abe2328bd35b07744074a
Camille Girodengo <115540569+Giro4c@users.noreply.github.com>
2023-10-26 08:20:46 +0200

    Test server paths

--- DataBase-Server/index.php
+++ DataBase-Server/index.php
@@ -3,6 +3,10 @@ session_start();
 $ip = $_SERVER['REMOTE_ADDR'];
 var_dump($ip);
 $plateforme = $_SERVER["HTTP_USER_AGENT"];
-var_dump($plateforme);
+//var_dump($plateforme);
 $date = date('Y-m-d H:i:s');
-var_dump($date);
+//var_dump($date);
+?>
+<form action="./views/test.php">
+    <button onclick="submit()">Aller vers Test</button>
+</form>
