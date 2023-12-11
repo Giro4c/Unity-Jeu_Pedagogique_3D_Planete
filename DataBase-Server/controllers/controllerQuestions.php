cb463372bf82c4780a2f175c5b4a6d99833c208a
Camille Girodengo <115540569+Giro4c@users.noreply.github.com>
2023-12-11 17:38:17 +0100

    Create controllerQuestions.php

---
+++ DataBase-Server/controllers/controllerQuestions.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace controllers;
+use database\DbJoueur;
+use database\DbPartie;
+use database\DbQuestion;
+use database\DbTypeques;
+use utilities\CannotDoException;
+
+class controllerQuestions
+{
+
+    private DbQuestion $dbQuestion;
+    private DbTypeques $dbTypeques;
+
+    public function __construct($conn){
+        $this->dbQuestion = new DbQuestion($conn);
+        $this->dbTypeques = new DbTypeques($conn);
+    }
+
+    public function getRandomQs(int $howManyQCM = 0, int $howManyInterac = 0, int $howManyVraiFaux = 0): string{
+        $totalQs = $howManyQCM + $howManyInterac + $howManyVraiFaux;
+        $numQs = $this->dbTypeques->getRandomQs();
+        ob_start(); ?>
+    <ul>
+        <?php for ($count = 0; $count < $totalQs; ++$count){?>
+        <li id="<?= $count ?>"><?= $numQs[$count] ?></li>
+        <?php }?>
+    </ul>
+    <?php
+        $contentHTML = ob_get_contents();
+        ob_end_clean();
+        return $contentHTML;
+    }
+
+    public function getHTMLAttributesQ(int $numQues): string{
+        $qAttributes = $this->dbTypeques->getQAttributes($numQues);
+        ob_start();
+        while ($attribute = current($qAttributes)) { ?>
+            <p id="<?= key($qAttributes) ?>"><?= $attribute ?></p>
+        <?php
+            next($array);
+        }
+        $qHTML = ob_get_contents();
+        ob_end_clean();
+        return $qHTML;
+    }
+
+    public function addFinishedQuestion(int $numQues, int $idParty, int $duration, bool $isCorrect): void{
+
+    }
+
+}
