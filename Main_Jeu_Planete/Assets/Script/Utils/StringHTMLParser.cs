c618d059e2dcf4f5afc093b3718147411d40a6fe
Camille Girodengo <115540569+Giro4c@users.noreply.github.com>
2023-12-11 11:14:31 +0100

    HTMLParser

---
+++ Main_Jeu_Planete/Assets/Script/Utils/StringHTMLParser.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StringHTMLParser
+{
+    private string html;
+    
+    StringHTMLParser(string html)
+    {
+        this.html = html;
+    }
+
+    public string getHTMLContainerContent(string type, string classContainer, string id)
+    {
+        return Between(html, buildContainerOP(type, classContainer, id), buildContainerED(type));
+    }
+    
+    public string Between(string str , string firstString, string lastString)
+    {
+        int pos1;
+        int pos2;
+        if (firstString == null || firstString.Equals(""))
+        {
+            pos1 = 0;
+        }
+        else
+        {
+            pos1 = str.IndexOf(firstString) + firstString.Length;
+        }
+        if (lastString == null || lastString.Equals(""))
+        {
+            pos2 = str.Length;
+        }
+        else
+        {
+            pos2 = str.IndexOf(lastString, pos1);
+        }
+        string finalString = str.Substring(pos1, pos2 - pos1);
+        return finalString;
+    }
+
+    public string buildContainerOP(string type, string classContainer, string id)
+    {
+        if (type == null || type.Equals("")) return null;
+        string container = "<" + type;
+        if (classContainer != null && ! classContainer.Equals(""))
+        {
+            container = container + " " + classContainer;
+        }
+        if (id != null && ! id.Equals(""))
+        {
+            container = container + " " + id;
+        }
+
+        container = container + ">";
+        return container;
+    }
+    
+    public string buildContainerED(string type)
+    {
+        if (type == null || type.Equals("")) return null;
+        string container = "</" + type + ">";
+        return container;
+    }
+    
+}
