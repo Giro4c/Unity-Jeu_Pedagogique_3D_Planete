65c7d31830ee022d910ffd7806d7b4d9f6d21bbc
Camille Girodengo <115540569+Giro4c@users.noreply.github.com>
2023-12-14 13:08:25 +0100

    Update ListQuestions.cs

--- Main_Jeu_Planete/Assets/Script/VarContainers/ListQuestions.cs
+++ Main_Jeu_Planete/Assets/Script/VarContainers/ListQuestions.cs
@@ -1,20 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ListQuestions : MonoBehaviour
 {
-    public int[] questionsIDs;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int[] questionsIDs = Array.Empty<int>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
