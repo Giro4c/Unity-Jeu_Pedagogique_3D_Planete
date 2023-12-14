0bf85d5dfe574bf38cf1a4ede298dbe7fbc4afef
Camille Girodengo <115540569+Giro4c@users.noreply.github.com>
2023-12-14 17:40:45 +0100

    Show Hour

--- Main_Jeu_Planete/Assets/Script/Behaviors/UI/CurrentHour.cs
+++ Main_Jeu_Planete/Assets/Script/Behaviors/UI/CurrentHour.cs
@@ -1,17 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System;
+using UnityEngine;
 using TMPro;
-
 
 public class CurrentHour : MonoBehaviour
 {
-    [SerializeField] TMP_Text currentHour;
+    [SerializeField] private TMP_Text displayText;
+    //public Orbit valueOrbit;
+    public RotationCycle valueRotationDay;
+    private float _hourFloat = 0f;
+    
+    private void Start()
+    {
+        UpdateHourAndText();
+    }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        UpdateHourAndText();
+    }
+
+    private void UpdateHourAndText()
+    {
+        _hourFloat = GetHourFloat();
+        displayText.text = GetHourText();
+    }
+
+    private int getHour(float hourDigit)
+    {
+        return (int) Math.Floor(hourDigit);
+    }
+
+    private int getMinutes(float hourDigit)
+    {
+        return (int) Math.Floor((hourDigit % 1f) * 60);
+    }
+
+    private float GetHourFloat()
+    {
+        return valueRotationDay.rotateProgress * 24;
+    }
+
+    private string GetHourText()
+    {
+        string text = "" + getHour(_hourFloat) + " : ";
+        int min = getMinutes(_hourFloat);
+        if (min < 10)
+        {
+            text += "0";
+        }
+
+        text += min;
+        return text;
     }
 }
