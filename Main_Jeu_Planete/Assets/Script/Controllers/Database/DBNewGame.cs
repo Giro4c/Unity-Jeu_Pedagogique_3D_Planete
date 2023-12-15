43a86ef4b0999a569a4e084435310a432bff88bf
Camille Girodengo <115540569+Giro4c@users.noreply.github.com>
2023-12-15 14:20:52 +0100

    Forgor plateforme indication in url

--- Main_Jeu_Planete/Assets/Script/Controllers/Database/DBNewGame.cs
+++ Main_Jeu_Planete/Assets/Script/Controllers/Database/DBNewGame.cs
@@ -9,6 +9,7 @@ using UnityEngine.UI;
 public class DBNewGame : MonoBehaviour
 {
     public Button buttonStart;
+    [SerializeField] public string plateforme = "Windows";
     
     // Start is called before the first frame update
     void Start()
@@ -24,7 +25,7 @@ public class DBNewGame : MonoBehaviour
 
     private IEnumerator NewGamePhp()
     {
-        string url = "jeupedagogique.alwaysdata.net/views/addNewGame.php";
+        string url = "jeupedagogique.alwaysdata.net/views/addNewGame.php?plateforme=" + plateforme;
         Debug.Log(url);
         
         UnityWebRequest wwwNewGame = UnityWebRequest.Get(url);
