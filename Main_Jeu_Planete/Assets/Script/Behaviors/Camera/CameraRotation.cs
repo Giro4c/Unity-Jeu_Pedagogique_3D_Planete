d590eb6044d2d5a39e9f9ee8ed8ed22dbee7f5d1
Camille Girodengo <115540569+Giro4c@users.noreply.github.com>
2023-12-17 22:02:17 +0100

    Fix Camera movement when clicking on UI Interface

--- Main_Jeu_Planete/Assets/Script/Behaviors/Camera/CameraRotation.cs
+++ Main_Jeu_Planete/Assets/Script/Behaviors/Camera/CameraRotation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CameraRotation : MonoBehaviour
 {
@@ -8,13 +9,14 @@ public class CameraRotation : MonoBehaviour
     private const float MAX_BORNE_X = 310;
     private const float MIN_BORNE_X = 50;
     private const float MEDIAN_BORNE_X = MAX_BORNE_X - MIN_BORNE_X;
+    private EventSystem _eventSys;
     
     public float speed = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _eventSys = GameObject.Find("EventSystem").GetComponent<EventSystem>();
     }
 
     // Update is called once per frame
@@ -22,30 +24,36 @@ public class CameraRotation : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            // Verifying if there is no object in front of the mouse on the screen
-            RaycastHit hit;
-            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            // Does the ray intersect any objects excluding the player layer
-            if (!Physics.Raycast(mouseRay, out hit))
+            // Verifying if there is no UI object in front of the mouse
+            if (_eventSys.Equals(null) || ! _eventSys.IsPointerOverGameObject())
             {
-                //Debug.Log("Did Not Hit");
+                // Verifying if there is no object in front of the mouse on the screen
+                RaycastHit hit;
+                Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                // Does the ray intersect any objects excluding the player layer
+                if (!Physics.Raycast(mouseRay, out hit))
+                {
+                    //Debug.Log("Did Not Hit");
                 
-                /* Change Camera angle :
-                 Mouse Up -> Look Up
-                 Mouse Down -> Look Down
-                 Mouse Left -> Look Right
-                 Mouse Right -> Look Left
-                */
-                Vector3 newAngle = transform.eulerAngles + ( speed * new Vector3(Input.GetAxis("Mouse Y"), -1 * Input.GetAxis("Mouse X"), 0));
-                newAngle.x = AdjustXAngleToBoundaries(newAngle.x);
-                transform.eulerAngles = newAngle;
+                    /* Change Camera angle :
+                     Mouse Up -> Look Up
+                     Mouse Down -> Look Down
+                     Mouse Left -> Look Right
+                     Mouse Right -> Look Left
+                    */
+                    Vector3 newAngle = transform.eulerAngles + ( speed * new Vector3(Input.GetAxis("Mouse Y"), -1 * Input.GetAxis("Mouse X"), 0));
+                    newAngle.x = AdjustXAngleToBoundaries(newAngle.x);
+                    transform.eulerAngles = newAngle;
+                }
+                else
+                {
+                    //Debug.Log("Did Hit");
+                } 
             }
             else
             {
-                //Debug.Log("Did Hit");
+                //Debug.Log("Cursor on UI object");
             }
-            
-            
 
         }
     }
