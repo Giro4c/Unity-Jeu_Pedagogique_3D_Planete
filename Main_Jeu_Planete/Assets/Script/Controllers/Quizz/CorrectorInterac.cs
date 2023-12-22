2bf0f8a9eed16ec583cc8991974c6c6ba15829a7
Camille Girodengo <115540569+Giro4c@users.noreply.github.com>
2023-12-22 15:51:16 +0100

    Debug Traces for Bug: Wrong answer when actually correct

--- Main_Jeu_Planete/Assets/Script/Controllers/Quizz/CorrectorInterac.cs
+++ Main_Jeu_Planete/Assets/Script/Controllers/Quizz/CorrectorInterac.cs
@@ -31,18 +31,26 @@ public class CorrectorInterac : QuestionCorrector
         bool orbitCorrect = true;
         if (verifyOrbit)
         {
+            Debug.Log("Upper Bound orbit : " + (correctOrbit + orbitMargin));
+            Debug.Log("Lower Bound orbit : " + (correctOrbit - orbitMargin));
+            Debug.Log("Orbit actuel : " + orbit.orbitProgress);
             if (correctOrbit + orbitMargin > 1 || correctOrbit - orbitMargin < 0)
             {
+                Debug.Log("Case OU pour intervalle Orbit");
+                Debug.Log("" + (correctOrbit - orbitMargin)%1f + " < orbit || orbit < " + (correctOrbit + orbitMargin)%1f);
                 orbitCorrect = orbit.orbitProgress < (correctOrbit + orbitMargin)%1f ||
                                orbit.orbitProgress > (correctOrbit - orbitMargin)%1f;
             }
             else
             {
+                Debug.Log("Case ET pour intervalle Orbit");
+                Debug.Log("" + (correctOrbit - orbitMargin) + " < orbit && orbit < " + (correctOrbit + orbitMargin)%1f);
                 orbitCorrect = orbit.orbitProgress < (correctOrbit + orbitMargin) &&
                                orbit.orbitProgress > (correctOrbit - orbitMargin);
             }
             
         }
+        Debug.Log("Orbit correcte ? " + orbitCorrect);
         
         // Vérifier si dans cas correctOrbit proche de 0 si intervalle est bon
         bool rotaCorrect = true;
@@ -51,21 +59,31 @@ public class CorrectorInterac : QuestionCorrector
             // Special case, require specific time (sun progress) but no particular orbit, must take current orbit into account
             if (!verifyOrbit)
             {
+                Debug.Log("Special case rotation : only time matters");
                 // Recalculate correct rotation based on actual orbit and correct value for orbit progress 0 (aka correct sun progress).
                 correctRotation += orbit.orbitProgress;
                 correctRotation %= 1f;
+                Debug.Log("Special case correct rotation : rotation=" + correctRotation +"  --  orbit=" + orbit.orbitProgress);
             }
+            Debug.Log("Upper Bound rotation : " + (correctRotation + rotationMargin));
+            Debug.Log("Lower Bound rotation : " + (correctRotation - rotationMargin));
+            Debug.Log("Rotation actuelle : " + rotation.rotateProgress);
             if (correctRotation + rotationMargin > 1 || correctRotation - rotationMargin < 0)
             {
+                Debug.Log("Case OU pour intervalle Rotation");
+                Debug.Log("" + (correctRotation - rotationMargin)%1f + " < rotation || rotation < " + (correctRotation + rotationMargin)%1f);
                 rotaCorrect = rotation.rotateProgress < (correctRotation + rotationMargin)%1f ||
                               rotation.rotateProgress > (correctRotation - rotationMargin)%1f;
             }
             else
             {
+                Debug.Log("Case ET pour intervalle Rotation");
+                Debug.Log("" + (correctRotation - rotationMargin) + " < rotation && rotation < " + (correctRotation + rotationMargin)%1f);
                 rotaCorrect = rotation.rotateProgress < (correctRotation + rotationMargin) &&
                               rotation.rotateProgress > (correctRotation - rotationMargin);
             }
         }
+        Debug.Log("Rotation correcte ? " + rotaCorrect);
         // Change value of correct
         SetCorrect(orbitCorrect && rotaCorrect);
         
