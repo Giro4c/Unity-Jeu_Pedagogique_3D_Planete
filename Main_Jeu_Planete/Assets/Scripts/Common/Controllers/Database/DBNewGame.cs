e158a14e8e550f2e6a8b2c45c435b7d89f9536d7
Maxime <115540245+JAURAS-Maxime-2225046a@users.noreply.github.com>
2024-01-15 11:29:24 +0100

    Detection XR/VR

--- Main_Jeu_Planete/Assets/Scripts/Common/Controllers/Database/DBNewGame.cs
+++ Main_Jeu_Planete/Assets/Scripts/Common/Controllers/Database/DBNewGame.cs
@@ -5,29 +5,36 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEngine.XR;
 
 public class DBNewGame : MonoBehaviour
 {
     public Button buttonStart;
     [SerializeField] public string plateforme = "Windows";
-    
+
     // Start is called before the first frame update
     void Start()
     {
         buttonStart.onClick.AddListener(delegate { StartCoroutine(NewGamePhp()); });
+
+        // Vérifier si la réalité virtuelle est activée
+        if (XRSettings.enabled)
+        {
+            plateforme = "XR";
+        }
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private IEnumerator NewGamePhp()
     {
         string url = "jeupedagogique.alwaysdata.net/views/addNewGame.php?plateforme=" + plateforme;
         Debug.Log(url);
-        
+
         UnityWebRequest wwwNewGame = UnityWebRequest.Get(url);
         yield return wwwNewGame.SendWebRequest();
         if (wwwNewGame.error != null)
@@ -39,8 +46,6 @@ public class DBNewGame : MonoBehaviour
             Debug.Log("Page New Game Loaded");
             Debug.Log(wwwNewGame.downloadHandler.text);
         }
-        
+
     }
-    
-    
 }
