1229ca641721031a66dc5258db3dd9660ad8984f
Yazid-Raoul Maoulida Attoumani <yazid-raoul.MAOULIDA-ATTOUMANI@etu.univ-amu.fr>
2024-01-17 12:38:33 +0100

    AxeTerminer

--- Main_Jeu_Planete/Assets/Scripts/Common/Behaviors/SolarSystem/AxeEllipse.cs
+++ Main_Jeu_Planete/Assets/Scripts/Common/Behaviors/SolarSystem/AxeEllipse.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class AxeEllipse : MonoBehaviour
 {
     
-    public float semiMajorAxis;
-    public float semiMinorAxis;
+    public Ellipse ellipse;
     public int segments = 100;
     public Color axisColor = Color.white;
 
@@ -15,11 +14,6 @@ public class AxeEllipse : MonoBehaviour
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        
-    }
-    void Update()
-    {
-        
         SetupAxis();
     }
 
@@ -27,23 +21,23 @@ public class AxeEllipse : MonoBehaviour
     {
         // Définir les propriétés du LineRenderer
         lineRenderer.positionCount = segments + 1;
-        lineRenderer.startWidth = 0.04f;
-        lineRenderer.endWidth = 0.04f;
+        lineRenderer.startWidth = 0.02f;
+        lineRenderer.endWidth = 0.02f;
         lineRenderer.material = new Material(Shader.Find("Standard"));
         lineRenderer.material.color = axisColor;
-        // Position choisie dans l'espace
-        Vector3 chosenPosition = new Vector3(104f, 6f, 108f);
 
-        // Dessiner l'axe principal de l'ellipse à la position choisie
+        // Dessiner l'axe principal de l'ellipse à la position de votre choix
+        Vector3 position = transform.position;  // Utilisez la position actuelle de l'objet ou définissez une position spécifique
+        DrawEllipse(position);
+    }
+
+    void DrawEllipse(Vector3 position)
+    {
         for (int i = 0; i <= segments; i++)
         {
-            float angle = 2f * Mathf.PI * i / segments;
-            float x = semiMajorAxis * Mathf.Cos(angle);
-            float y = semiMinorAxis * Mathf.Sin(angle);
-            
-            // Ajouter la position choisie à chaque point
-            Vector3 point = new Vector3(x-1, 0f, y+1) + chosenPosition;
-
+            float t = i / (float)segments;
+            Vector2 pointOnEllipse = ellipse.Evaluate(t);
+            Vector3 point = new Vector3(pointOnEllipse.x + position.x, position.y, pointOnEllipse.y + position.z);
             lineRenderer.SetPosition(i, point);
         }
     }
