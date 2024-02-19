a345e02cf82051164da719092419b594b76ec5b9
Maxime <115540245+JAURAS-Maxime-2225046a@users.noreply.github.com>
2024-02-19 12:00:08 +0100

    Update domain in MVC

---
+++ DataBase-Server/domain/Quesinterac.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace domain;
+
+class Quesinterac extends Question
+{
+    protected $BonneRepValeur_orbit;
+    protected $Marge_Orbit;
+    protected $BonneRepValeur_rotation;
+    protected $Marge_Rotation;
+
+    public function __construct($Num_Ques, $BonneRepValeur_orbit, $Marge_Orbit, $BonneRepValeur_rotation, $Marge_Rotation)
+    {
+        parent::__construct($Num_Ques, '', '');
+        $this->BonneRepValeur_orbit = $BonneRepValeur_orbit;
+        $this->BonneRepValeur_rotation = $BonneRepValeur_rotation;
+        $this->Marge_Orbit = $Marge_Orbit;
+        $this->Marge_Rotation = $Marge_Rotation;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBonneRepValeurOrbit()
+    {
+        return $this->BonneRepValeur_orbit;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMargeOrbit()
+    {
+        return $this->Marge_Orbit;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBonneRepValeurRotation()
+    {
+        return $this->BonneRepValeur_rotation;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMargeRotation()
+    {
+        return $this->Marge_Rotation;
+    }
+}
---
+++ DataBase-Server/domain/VraiFaux.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace domain;
+
+class VraiFaux extends Question
+{
+    protected $Valeur_orbit;
+    protected $Valeur_rotation;
+    protected $BonneRep;
+
+    public function __construct($Num_Ques, $Valeur_orbit, $Valeur_rotation, $BonneRep)
+    {
+        parent::__construct($Num_Ques, '', '');
+        $this->Valeur_orbit = $Valeur_orbit;
+        $this->BonneRep = $BonneRep;
+        $this->Valeur_rotation = $Valeur_rotation;
+    }
+
+    public function getValeurOrbit()
+    {
+        return $this->Valeur_orbit;
+    }
+
+    public function getValeurRotation()
+    {
+        return $this->Valeur_rotation;
+    }
+
+    public function getBonneRep()
+    {
+        return $this->BonneRep;
+    }
+}
