e305a3dbac7a2cf0f26d21eaf37c018375deac3b
Yazid-Raoul Maoulida Attoumani <yazid-raoul.MAOULIDA-ATTOUMANI@etu.univ-amu.fr>
2024-02-19 04:02:06 +0100

    AxeTerreModifier

--- Jeu_Planete_3D/Assets/Scripts/Common/Behaviors/SolarSystem/AxeTerre.cs
+++ Jeu_Planete_3D/Assets/Scripts/Common/Behaviors/SolarSystem/AxeTerre.cs
@@ -2,43 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(LineRenderer))]
+
 public class AxeTerre : MonoBehaviour
-{
-    private int numSegments = 50;
-    public float radius = 1f;
-    
+{ 
+    // Référence au GameObject représentant la Terre
+    public GameObject terre;
+
+    // Épaisseur de la ligne
+    public float lineWidth = 0.1f;
+
+    // Longueur de la ligne
+    public float lineLength = 2.0f;
+
+    // Angle d'inclinaison en degrés
+    public float inclinationAngle = 23f;
+
+    // Couleur de la ligne
+    public Color lineColor = Color.green;
 
     private LineRenderer lineRenderer;
 
-    private void Start()
+    void Start()
     {
-        lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.positionCount = numSegments + 1;
-        lineRenderer.startWidth = 0.1f;
-        lineRenderer.endWidth = 0.1f;
-        
-    }
-
-    private void Update()
-    {
-        DrawCircle();
-    }
-
-    private void DrawCircle()
-    {
-        float deltaTheta = (2f * Mathf.PI) / numSegments;
-        float theta = 0f;
-
-        for (int i = 0; i < numSegments + 1; i++)
+        // Vérifie si la référence à la Terre est définie
+        if (terre == null)
         {
-            float x = radius * Mathf.Cos(theta);
-            float z = radius * Mathf.Sin(theta);
+            Debug.LogError("La référence à la Terre n'est pas définie.");
+            return;
+        }
 
-            Vector3 pos = new Vector3(x, 0f, z) + transform.position;
-            lineRenderer.SetPosition(i, pos);
+        // Crée un nouveau GameObject pour représenter la ligne
+        GameObject verticalLine = new GameObject("VerticalLine");
 
-            theta += deltaTheta;
+        // Assigne la Terre comme parent de la ligne
+        verticalLine.transform.parent = terre.transform;
+
+        // Place la ligne au centre de la Terre
+        verticalLine.transform.localPosition = Vector3.zero;
+
+        // Ajoute le composant LineRenderer à la ligne
+        lineRenderer = verticalLine.AddComponent<LineRenderer>();
+
+        // Définit les paramètres de la ligne
+        lineRenderer.startWidth = lineWidth;
+        lineRenderer.endWidth = lineWidth;
+        lineRenderer.positionCount = 2;
+        lineRenderer.startColor = lineColor;
+        lineRenderer.endColor = lineColor;
+
+        // Incline la ligne autour de l'axe X
+        verticalLine.transform.rotation = Quaternion.Euler(inclinationAngle, 0f, 0f);
+    }
+
+    void Update()
+    {
+        if (terre != null && lineRenderer != null)
+        {
+            // Met à jour la position de la ligne pour suivre la Terre
+            lineRenderer.SetPosition(0, terre.transform.position + Vector3.down * lineLength / 2);
+            lineRenderer.SetPosition(1, terre.transform.position + Vector3.up * lineLength / 2);
         }
     }
 }
