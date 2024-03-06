3d2769260ede6b25cb37beeec48278fcbd36cefa
Xpery <martinezromain622@gmail.com>
2024-03-06 15:47:25 +0100

    test

--- DataBase-Server/control/ControllerQuestions.php
+++ DataBase-Server/control/ControllerQuestions.php
@@ -35,8 +35,8 @@ class ControllerQuestions
      */
     public function getJsonAttributesQ(int $numQues, PartieChecking $questionService, mixed $data): string {
         $qAttributes = $questionService->getQAttributes($numQues, $data);
+        var_dump($qAttributes);
         var_dump(json_encode($qAttributes, JSON_PRETTY_PRINT));
-
         return json_encode($questionService->getQAttributes($numQues, $data), JSON_PRETTY_PRINT);
     }
 
